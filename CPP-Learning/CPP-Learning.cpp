f7c4c150fc5c9c3504d6fef6e43b3044435a5e76
parallela <lubomir.c.stankov@gmail.com>
2020-09-18 16:57:37 +0300

    Simple function calling

--- CPP-Learning/CPP-Learning.cpp
+++ CPP-Learning/CPP-Learning.cpp
@@ -3,32 +3,48 @@
 using namespace std;
 
 
-int main()
-{
-	// Work with variables
-	//char gradudation = 'A';
-	//string name = "Lubomir";
-	//int age = 50;
-	//double cm = 2.4
-	int age = 0;
-	//bool isMale = true/false
-
-	/*
-	 * HelloWorld
-	 *
-	 * cout << "Hello World \n";
-	 */
-
-	
-	//cout << "Hello " << name << endl;
-	//cout << gradudation << endl;
-	//cout << phrase.find("mir", 0);
-	//cout << phrase.substr(0,8);
-
-	cout << "Enter Your Age: ";
-	cin >> age;
-
-	cout << "Your ages are " << age << endl;
-	
-	return 0;
+bool itsDevidable(int number) {
+    if (number < 0) {
+        return false;
+    }
+//    cout << (number % 7) << endl;
+    if (!(number % 7)) {
+        cout << "It's devisible by 7 \n";
+        return true;
+    } else {
+        cout << "It's not devisible by 7, try again \n";
+        return false;
+    }
 }
+
+int main() {
+    // Work with variables
+    //char gradudation = 'A';
+    //string name = "Lubomir";
+    //int age = 50;
+    //double cm = 2.4
+    //int age = 0;
+    //bool isMale = true/false
+    //	string words[] = { "word1", "word2", "word3"};
+    /*
+     * HelloWorld
+     *
+     * cout << "Hello World \n";
+     */
+
+
+    //cout << "Hello " << name << endl;
+    //cout << gradudation << endl;
+    //cout << phrase.find("mir", 0);
+    //cout << phrase.substr(0,8);
+
+    //cout << "Enter Your Age: ";
+    //cin >> age;
+    //
+    //cout << "Your ages are " << age << endl;
+    itsDevidable(28);
+
+
+    return 0;
+}
+
---
+++ cmake-build-debug/CMakeFiles/CPP_Learning.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/CPP_Learning.dir/CPP-Learning/CPP-Learning.cpp.obj"
+  "CPP_Learning.exe"
+  "CPP_Learning.exe.manifest"
+  "CPP_Learning.lib"
+  "CPP_Learning.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/CPP_Learning.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
