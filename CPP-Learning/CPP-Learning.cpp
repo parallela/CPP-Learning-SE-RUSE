fdf5116d8b15502dd9533d685bb26ad1f3fc74e2
parallela <lubomir.c.stankov@gmail.com>
2020-09-23 13:56:33 +0300

    Training some exercies

--- CPP-Learning/CPP-Learning.cpp
+++ CPP-Learning/CPP-Learning.cpp
@@ -3,13 +3,16 @@ using namespace std;
 #include <iostream>
 #include <stdlib.h>
 #include <conio.h>
+#include <algorithm>
+#include <vector>
 
 int main() {
-    float a=7.5, b=2; char x='a', y='q', z='r';
-    float asd = x+y < z;
+    vector<int> numbers = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+    sort(numbers.begin(), numbers.end());
 
-    float m=(a<b||x+y<z)+a/b-1;
-    cout<<m<<endl;
+    cout << numbers[0] + numbers[1] << endl;
+
+
     return 0;
-
 }
+
