86295a12474bad3ae53d95e68869ebaad4798ca3
parallela <lubomir.c.stankov@gmail.com>
2020-09-25 21:21:53 +0300

    Remove all vowels exercies

--- CPP-Learning/CPP-Learning.cpp
+++ CPP-Learning/CPP-Learning.cpp
@@ -1,33 +1,27 @@
 using namespace std;
 
 #include <iostream>
-#include <stdlib.h>
-#include <conio.h>
 #include <algorithm>
 #include <vector>
 
-int main() {
-    string Text = "HELLO";
-    transform(Text.begin(), Text.end(), Text.begin(), ::tolower);
+string removeVowels(string text) {
+    vector<char> vowels = {
+            'a', 'e', 'i', 'o', 'u',
+            'A', 'E', 'I', 'O', 'U'
+    };
 
-    string temp = Text;
-
-    vector <string> WaveVector = {};
-
-
-    for (int i = 0; i < Text.size(); ++i) {
-        if (Text[i] == ' ') continue;
-        temp[i] = toupper(temp[i]);
-        WaveVector.push_back(temp);
-        cout << temp << endl;
-        temp = Text;
+    for (int i = 0; i < text.length(); i++) {
+        if (find(vowels.begin(), vowels.end(), text[i]) != vowels.end()) {
+            text = text.replace(i, 1, "");
+            i -= 2;
+        }
     }
 
-//    for (int i = 0; i < WaveVector.size(); ++i) {
-//        cout << WaveVector[i] << endl;
-//    }
+    return text;
+}
 
+int main() {
+    cout << removeVowels("This website is for losers LOL!") << endl;
 
     return 0;
 }
-
