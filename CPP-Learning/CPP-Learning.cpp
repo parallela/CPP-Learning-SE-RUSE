6f64d3ff1d8d5558c4bddc804f75fab16f3904b6
parallela <lubomir.c.stankov@gmail.com>
2020-10-06 16:27:04 +0300

    Homework exercies (Find if point lies on rectangle)

--- CPP-Learning/CPP-Learning.cpp
+++ CPP-Learning/CPP-Learning.cpp
@@ -1,32 +1,48 @@
 using namespace std;
 
 #include <iostream>
-#include <algorithm>
-#include <sstream>
-#include <unordered_set>
-#include <vector>
+#include<windows.h>
 
-int main() {
-    vector<int> chisla = {2, 1, 4, 5, 6, 7, 8, 0, 0};
-    vector<int> chetni_chisla = {};
-    vector<int> nechetni_chisla = {};
-    int nulevi_stoinosti = 0;
+bool nameri_tochkata(int x1, int y1, int x2, int y2, int x, int y) {
 
-
-    for (int i = 0; i < chisla.size(); ++i) {
-        if (chisla[i] == 0) {
-            nulevi_stoinosti += 1;
-        }
-        if (chisla[i] % 2 == 0 && chisla[i] != 0) {
-            chetni_chisla.push_back(chisla[i]);
-        } else if (chisla[i] != 0 && chisla[i] % 2 != 0) {
-            nechetni_chisla.push_back(chisla[i]);
-        }
-
+    // Проверяване дали точката лежи в/у триъгълника.
+    if (x > x1 && x < x2 && y > y1 && y < y2) {
+        return true;
     }
 
-    for (int i = 0; i < nechetni_chisla.size(); ++i) {
-        cout << nechetni_chisla[i] << endl;
+    return false;
+}
+
+int main() {
+    // Промяна на encoding-а на конзолата
+    SetConsoleOutputCP(1251);
+    SetConsoleCP(1251);
+
+    // Иниц.. на променливите
+    int x1 = 0, y1 = 0, x2 = 0, y2 = 0, x, y;
+
+    // Въвеждане на долните и горните леви и десни ъгли на правоъгълника.
+    cout << "Въведи x1: ";
+    cin >> x1;
+    cout << "Въведи y1: ";
+    cin >> y1;
+    cout << "Въведи x2: ";
+    cin >> x2;
+    cout << "Въведи y2: ";
+    cin >> y2;
+
+
+    // Въвеждане на точката
+    cout << "Въведи x: ";
+    cin >> x;
+    cout << "Въведи y: ";
+    cin >> y;
+
+    // Проверяване на точката според кординатите на тригълника
+    if (nameri_tochkata(x1, y1, x2, y2, x, y)) {
+        cout << "Точката лежи в правоъгалника (Da) \n";
+    } else {
+        cout << "Точката не лежи в правоъгалника (Ne)\n";
     }
 
     return 0;
